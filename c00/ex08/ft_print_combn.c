b041ab78188166a4326acdeb0e164c9e629c47db
Bryan Bong <bbong@u81z04s04.42kl.edu.my>
2021-06-28 18:26:21 +0800

    add ex08 and a.out to files

---
+++ c00/ex08/ft_print_combn.c
@@ -0,0 +1,76 @@
+#include <unistd.h>
+
+void	putnbr(int a)
+{
+	a = (a % 10) + 48;
+	write (1, &a, 1);
+}
+
+void	set_array(int *a, int n)
+{
+	int counter;
+	int setnum;
+
+	counter = n - 1;
+	setnum = 0;
+	while (counter >= 0)
+	{
+		a[counter] = setnum;
+		counter--;
+		setnum++;
+	}
+}
+
+void	print(int *a, int n)
+{
+	int counter;
+	counter = n - 1;
+	while (counter >= 0)
+	{
+		putnbr(a[counter]);
+		counter--;
+	}
+	if (a[n - 1] != 10 - n)
+	{
+		write(1, ",", 1);
+		write(1, " ", 1);
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	a[n];
+	int i;
+	int counter;
+	int max;
+	int max2;
+	int temp = 0;
+
+	set_array(a, n);
+	while (a[n - 1] < 10 - n)
+	{
+		print(a, n);
+		i = 0;
+		max = 10;
+		a[i]++;
+		while (a[i] == max && i < n)
+		{
+			counter = i;
+			a[i + 1]++;
+			while (counter >= 0)
+			{
+				a[counter] = a[counter + 1] + 1;
+				counter--;
+			}
+			i++;
+			max--;
+		}
+	}
+	print(a, n);
+}
+
+int	main(void)
+{
+	ft_print_combn(2);
+	return (0);
+}
